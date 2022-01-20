89199d57bb302a2e1e32ac36125d140f2a085f7a
Ashenafi A Gebretekle <61099470+Ashe-Gebre@users.noreply.github.com>
2022-01-20 11:27:29 -0600

    AWS Amplify

---
+++ University-experience-sharing/frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "studentpostapp0f445b8b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
